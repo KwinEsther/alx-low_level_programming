1d90306d1e3bd3ac784bf6abe3a02eca967a94f0
KwinEsther <esthernkanta6@gmail.com>
2023-12-18 04:08:06 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
